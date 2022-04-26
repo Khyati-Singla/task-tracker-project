d4c5c048db723641fea724919949d3e0220b906a
Khyati-Singla <87181517+Khyati-Singla@users.noreply.github.com>
2022-04-26 12:03:05 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import Button from './Button';
+
+const Header = ({ title, onAdd, showAdd }) => {
+    
+  return <div>
+      <header className='header'>
+          <h1>{title}</h1>
+          <Button color = {showAdd ? 'red' : 'green'} text = {showAdd ? 'close' : 'Add'} onClick = {onAdd}/>
+          
+      </header>
+  </div>;
+};
+
+Header.defaultProps = {
+    title : 'Task Tracker',
+}
+
+Header.propTypes ={
+    title : propTypes.string.isRequired,
+}
+
+export default Header;
+
